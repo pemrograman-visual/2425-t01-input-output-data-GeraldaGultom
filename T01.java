6b2ac50722e178d5a83d570d5ab0a8a6c942014d
GeraldaGultom <geraldagultom@gmail.com>
2024-09-06 11:04:20 +0700

    Telah menyelesaikan tugas input-output data

--- T01.java
+++ T01.java
@@ -1,1 +1,35 @@
-// NIM - Your Name
+// 12S24044 - DIANITA LORENSIA BR GINTING
+// 12S24051 - GERALDA NATALI GULTOM
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN;
+        String judul;
+        String penulis;
+        String tahunTerbit;
+        String penerbit;
+        String formatBukuElektronik;
+        double hargaPembelian;
+        double minimumMargin;
+        int stok;
+        double rating;
+
+        iSBN = input.nextLine();
+        judul = input.nextLine();
+        penulis = input.nextLine();
+        tahunTerbit = input.nextLine();
+        penerbit = input.nextLine();
+        formatBukuElektronik = input.nextLine();
+        hargaPembelian = input.nextDouble();
+        minimumMargin = input.nextDouble();
+        stok = input.nextInt();
+        rating = input.nextDouble();
+        System.out.println(iSBN + "|" + judul + "|" + penulis + "|" + tahunTerbit + "|" + penerbit + "|" + formatBukuElektronik + "|" + hargaPembelian + "|" + minimumMargin + "|" + stok + "|" + rating);
+    }
+}
+
+
